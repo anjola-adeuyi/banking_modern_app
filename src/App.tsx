4496c9360fa5380a54046d73859db6d76615f476
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-09-19 23:15:23 +0100

    tailwind config fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <h1>Hello, World</h1>
+    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  </div>
+);
 
 export default App;
